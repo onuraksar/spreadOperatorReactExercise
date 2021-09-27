af6398ea86292240484b141485c339589f6fe3bf
Onur AKŞAR <onuraksar96@gmail.com>
2021-09-27 16:51:42 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    // use name key in [] while returning it inside an object below.
+    setContact((prevValue) => {
+      console.log("prevValue", prevValue);
+      console.log("name:", name);
+      console.log("value:", value);
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
